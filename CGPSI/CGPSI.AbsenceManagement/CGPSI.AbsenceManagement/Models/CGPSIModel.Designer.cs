8f88ce26ba4cb23907594e2abfba14045b849d9a
awidionugroho <adi.widionugroho@gmail.com>
2015-09-06 19:49:39 +0700

    update

--- CGPSI/CGPSI.AbsenceManagement/CGPSI.AbsenceManagement/Models/CGPSIModel.Designer.cs
+++ CGPSI/CGPSI.AbsenceManagement/CGPSI.AbsenceManagement/Models/CGPSIModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\MY PROJECTS\Ref\CGPSI\CGPSI.AbsenceManagement\CGPSI.AbsenceManagement\Models\CGPSIModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\INSPIRO-PC\Documents\Adi\Project\Source\Repos\CGPSI\CGPSI\CGPSI.AbsenceManagement\CGPSI.AbsenceManagement\Models\CGPSIModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
